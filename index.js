1ba91907327c821ad2219d8dfe146f122f0f566e
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-15 15:14:02 -0400

    Send data and handle response

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email) {
+
+  const userData = {
+    name: `${name}`,
+    email: `${email}`,
+  }
+
+  return fetch('http://localhost:3000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body:JSON.stringify(userData)
+    }).then(res => res.json()).then(appendData)
+}
+
+function appendData(userData) {
+  const body = document.querySelector('body');
+  body.innerHTML = userData.id
+}
